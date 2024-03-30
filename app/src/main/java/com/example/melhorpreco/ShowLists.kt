1d206dbbb0f14efba07ddb9430880624dade0b07
MatheusSev <matheus.severino@edu.unifil.br>
2024-03-29 22:15:46 -0300

    dios mio

---
+++ app/src/main/java/com/example/melhorpreco/ShowLists.kt
@@ -0,0 +1,12 @@
+package com.example.melhorpreco
+
+import android.app.Activity
+import android.os.Bundle
+
+
+class ShowLists:Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_showlists)
+    }
+}
